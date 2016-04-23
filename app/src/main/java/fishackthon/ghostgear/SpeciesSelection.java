c11ce060fb9bce4b1576708eac1adf3956bc1899
artLopez <arlopez@csumb.edu>
2016-04-23 03:30:59 -0700

    Changing way to add species

--- app/src/main/java/fishackthon/ghostgear/SpeciesSelection.java
+++ app/src/main/java/fishackthon/ghostgear/SpeciesSelection.java
@@ -4,41 +4,53 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Button;
+import java.util.ArrayList;
 
 public class SpeciesSelection extends AppCompatActivity {
-    private Button addButton;
     private Spinner speciesSelection;
-    private static final String[]species = {"Crab","Fish","Sea Lion", "Turtle"};
+    private static final String[]species = {"Select an species...","Crab","Fish","Sea Lion", "Turtle"};
     private ListView animalList;
-
+    private ArrayList<String> speciesAdded;
+    ArrayAdapter<String> speciesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_species_selection);
 
-        addButton = (Button) findViewById(R.id.addButton);
         speciesSelection = (Spinner) findViewById(R.id.speciesSelection);
         animalList = (ListView) findViewById(R.id.speciesList);
+        speciesAdded = new ArrayList<String>();
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
                 ( SpeciesSelection.this, android.R.layout.simple_spinner_item, species );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         speciesSelection.setAdapter(adapter);
+        speciesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, speciesAdded);
+        animalList.setAdapter( speciesAdapter );
 
-        addButton.setOnClickListener(new View.OnClickListener() {
+        speciesAdded.add(speciesSelection.getSelectedItem().toString());
+        speciesAdapter.notifyDataSetChanged();
+
+        speciesSelection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                String text =  speciesSelection.getSelectedItem().toString();
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                parentView.getItemAtPosition(position).toString();
+            }
 
-
-
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
             }
         });
     }
+
 }
+
+
 
